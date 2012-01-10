8d4c3fcfa88c4af1415224ad92cb5e33d14903c2
kardigen <kardigen@gmail.pl>
2012-01-11 00:54:04 +0100

    js basics constructor issues fixed.

--- js_basics.js
+++ js_basics.js
@@ -1,5 +1,5 @@
 
-console.log('How to create object?')
+console.log("\n How to create object?")
 var a = { 
 	text: 'some text', 
 	print: function(){ 
@@ -7,35 +7,39 @@ var a = {
 	}
 }
 a.print()
-
-
-
-console.log('Object-Functional oriented language?')
-var b = function() {
-	this.setText = function() {
-		this.text = 'ok another boring text...';
-	}
-	return this;
-}
-var c = b()
-c.setText()
-console.log('c.text: ',c.text)
 console.log('a: ',a)
-console.log('b: ',b)
 
 
+console.log("\n Object-Functional oriented language?")
+var b = function() {
+	console.log('function b called')
+}
 
-console.log('OK, so how to do inheritance?')
-b.prototype = a
-var d = new b()
+b.setText = function() {
+  b.text = 'ok another boring text...';
+}
+b()
+console.log('b.setText:',b.setText);
+console.log('b.text before setText call: ',b.text);
+b.setText()
+console.log('c.text after setText call: ',b.text)
+
+console.log("\n OK, so how to do inheritance?")
+
+var B = function() {
+  this.setText = function() {
+    this.text = 'nice text';
+  }
+}
+B.prototype = a
+var d = new B()
 d.print()
 d.setText()
 d.print()
-console.log('c.print: ',c.print)
 
 
 
-console.log('OK, inheritance looks strange, but what it gives me?')
+console.log("\n OK, inheritance looks strange, but what it gives me?")
 delete a.print
 console.log('d.print: ',d.print)
 
